b599b37798f8860d567ebd50a6775ff65f16f05d
jaisonrobson <xd_jrg@hotmail.com>
2021-12-14 22:57:01 -0300

    feat: adicionado tab navigator em cima do stack navigator

--- App.js
+++ App.js
@@ -2,12 +2,13 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
-import { MainStackNavigator } from "./app/navigation/StackNavigator";
+import BottomTabNavigator from "./app/navigation/TabNavigator";
+
 
 export default function App() {
   return (
     <NavigationContainer>
-      <MainStackNavigator />
+      <BottomTabNavigator />
     </NavigationContainer>
   );
 }
